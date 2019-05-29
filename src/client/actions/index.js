30064f3dfb05f6e7c6142fae389dbe9a5578b897
bhautik <bhautikng143@gmail.com>
2019-05-29 13:04:28 +0530

    post up

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -4,7 +4,7 @@ export const POST_STORE = "post_store";
 
 export const postStore = () => async dispatch => {
   const res = await axios.get(
-    "http://localhost/wordpress/wp-json/wp/v2/posts?per_page=100"
+    "https://rtcamp-blogapp.000webhostapp.com/wp-json/wp/v2/posts/?per_page=100"
   );
   //https://rtcamp-blogapp.000webhostapp.com/wp-json/wp/v2/posts/
   //http://localhost/wordpress/wp-json/wp/v2/posts
