cac39414210b016a17521433d708fe079a808942
bhautik <bhautikng143@gmail.com>
2019-05-29 12:56:58 +0530

    post update

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -4,7 +4,7 @@ export const POST_STORE = "post_store";
 
 export const postStore = () => async dispatch => {
   const res = await axios.get(
-    "https://rtcamp-blogapp.000webhostapp.com/wp-json/wp/v2/posts"
+    "http://localhost/wordpress/wp-json/wp/v2/posts?per_page=100"
   );
   //https://rtcamp-blogapp.000webhostapp.com/wp-json/wp/v2/posts/
   //http://localhost/wordpress/wp-json/wp/v2/posts
@@ -13,3 +13,6 @@ export const postStore = () => async dispatch => {
     payload: res
   });
 };
+//"dev:serve": "nodemon build/bundle.js",
+//  "start": "node build/bundle.js",
+//"test": "jest"
