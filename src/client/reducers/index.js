4e77b2b29e511fe9d2a55013449733288d8c527e
bhautik <bhautikng143@gmail.com>
2019-06-17 09:50:43 +0530

    deleted old project

--- src/client/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import postReducers from "./postReducers";
-
-export default combineReducers({
-  posts: postReducers
-});
--- src/createStore.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import thunk from "react-thunk";
-import reducers from "./reducer";
-
-export default () => {
-  const store = createStore(reducers, {}, applyMiddleware(thunk));
-
-  return store;
-};
